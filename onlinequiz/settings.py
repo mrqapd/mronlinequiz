c09c0f1e9dc9801e2401c2147152fa6c13ddc7d2
unknown <privatesidd1311@gmail.com>
2024-02-04 13:59:24 +0530

    first commit

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -25,13 +25,13 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
